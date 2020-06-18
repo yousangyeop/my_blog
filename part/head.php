d9c84c5df86a1a683e53ac3e3066662f391f1dac
yousangyeop <yeop4030@naver.com>
2020-06-18 18:51:52 +0900

    로고 파비콘 등록

--- part/head.php
+++ part/head.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>블로그</title>
+    <link rel="icon" href="../resource/img/favicon.ico">
+    <link rel="icon" href="../resource/img/blog.logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link rel="stylesheet" href="/resource/common.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -25,11 +27,11 @@
             <ul>
                 <li><a href="#" class="block">Home</a></li>
                 <li><a href="#" class="block">Articles</a>
-            <ul>
-                <li><a href="#">Notice</a></li>
-                <li><a href="#">Fee</a></li>
-            </ul>
-            </li>
+                    <ul>
+                        <li><a href="#">Notice</a></li>
+                        <li><a href="#">Fee</a></li>
+                    </ul>
+                </li>
                 <li><a href="#" class="block">About Me</a></li>
                 <li><a href="#" class="block">SNS</a>
                     <ul>
@@ -45,7 +47,7 @@
     <div class="top-bar visible-md-up">
         <div class="con height-100p flex">
             <a href="/" class="logo flex flex-ai-c">
-                <i class="fas fa-coffee"></i>
+                <img src="../resource/img/blog.logo.png" alt="" width="40">
             </a>
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
@@ -72,10 +74,10 @@
                             <li>
                                 <a href="https://www.instagram.com/yeobb/" target="_blank">
                                     INSTA
-                                <ul>
-                                    <li><a href="#">Private</a></li>
-                                    <li><a href="#">Public</a></li>
-                                </ul>
+                                    <ul>
+                                        <li><a href="#">Private</a></li>
+                                        <li><a href="#">Public</a></li>
+                                    </ul>
                                 </a>
                             </li>
                         </ul>
