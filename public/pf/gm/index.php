1144957e65ae6ca06094c6c1f1dab0bcfef37acd
yousangyeop <yeop4030@naver.com>
2020-07-08 18:45:33 +0900

    메뉴 만들기

---
+++ public/pf/gm/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./index.css">
+    <script src="./index.js"></script>
+</head>
+
+<body>
+    <nav class="flex con">
+        <div class="menu-box con flex flex-ai-c flex-1-0-0">
+            <a class="logo flex flex-1-0-0 flex-ai-c" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 166 15.9">
+                    <path
+                        d="M7.3 5.3V4.1c0-1.3-.6-1.9-2.1-1.9-1.6 0-2.2.6-2.2 1.9v7.7c0 1.3.5 1.9 2.1 1.9 1.5 0 2.2-1 2.2-2.5V9.4H4.8V7.2h5.5v8.5H7.7v-1.2c-.5.7-1.4 1.5-3.5 1.5C1.5 16 0 14.9 0 11.3V4.8C0 1.3 2.1 0 5.2 0c3 0 5.1 1.4 5.1 4.1v1.2h-3zm14.9-5v2.3h-6v3.9h5.4v2.2h-5.4v4.7h6v2.3h-9V.3h9zm13.5 0v15.3H32L28 3.9v11.7h-3V.3h3.7l4 11.6V.3h3zm12.5 0v2.3h-3.8v13.1h-3V2.6h-3.8V.3h10.6zm4.9 13.1H59v2.3h-8.9V.3h3v13.1zM69.9.3v2.3h-6v3.9h5.4v2.2h-5.4v4.7h6v2.3h-9V.3h9zM91 .3v15.3h-3V3.9l-2.6 11.8H83L80.5 3.8v11.9h-3V.3h4.2L84.2 12 86.7.3H91zM104.1 5v5.9c0 3.7-1.8 5-5.1 5s-5.1-1.2-5.1-5V5c0-3.7 1.8-5 5.1-5s5.1 1.3 5.1 5zm-3-.7c0-1.6-.6-2.1-2.1-2.1-1.6 0-2.1.5-2.1 2.1v7.4c0 1.6.6 2 2.1 2 1.6 0 2.1-.5 2.1-2V4.3zm16.7-4v15.3h-3.7l-4-11.7v11.7h-3V.3h3.7l4 11.6V.3h3zm12.3 3.6v.9h-3V4c0-1.3-.2-1.8-1.7-1.8-1.4 0-1.7.6-1.7 1.6 0 .9.2 1.3 1 1.8l2.9 1.9c1.6 1 2.5 1.6 2.5 4.4 0 2.7-1.8 4-4.7 4-3.5 0-4.9-1.2-4.9-4v-1.3h3v1.1c0 1.6.5 2 1.9 2s1.7-.6 1.7-1.7-.2-1.5-1.2-2.2L123 7.9c-1.8-1.2-2.4-1.8-2.4-4 0-1.9 1.1-3.8 4.7-3.8 3.7-.1 4.8 1.5 4.8 3.8M142.2.3v2.3h-3.8v13.1h-3V2.6h-3.8V.3h10.6zm10.9 0v2.3h-6v3.9h5.4v2.2h-5.4v4.7h6v2.3h-9V.3h9zm12.4 10.2v3.3c0 .8.2 1.5.6 1.9H163c-.3-.4-.5-1.1-.5-1.9v-3.3c0-1.2-.5-1.6-1.6-1.6h-2.1v6.8h-3V.3h5.5c4 0 4.5 2.2 4.5 4.3 0 1.7-.9 2.9-3.1 3.3 1.9.1 2.8 1.1 2.8 2.6zm-4.6-3.9c1.6 0 1.9-.7 1.9-2s-.2-2-1.9-2h-2.1v4h2.1z"
+                        fill="#000">
+                    </path>
+                </svg>
+            </a>
+        </div>
+
+        <div class="menu flex flex-jc-c">
+            <ul class="flex flex-ai-c">
+                <li class="flex"><a href="#">선글라스</a></li>
+                <li class="flex"><a href="#">안경</a></li>
+                <li class="flex"><a href="#">액세서리</a></li>
+                <li class="flex"><a href="#">콜라보레이션</a></li>
+                <li class="flex"><a href="#">매장</a></li>
+                <li class="flex"><a href="#">스토리</a></li>
+                <li class="flex"><a href="#">서비스</a></li>
+            </ul>
+        </div>
+
+        <div class="log-in flex flex-jc-end flex-1-0-0 con">
+            <ul class="flex flex-ai-c">
+                <li class="flex"><a href="#" class="flex">로그인</a></li>
+                <li class="flex"><a href="#" class="flex"><img
+                            src="https://yousangyeop.github.io/img1/blog/article/2020/look.png" alt=""></a></li>
+                <li class="flex"><a href="#" class="flex"><img
+                            src="https://yousangyeop.github.io/img1/blog/article/2020/read.png" alt=""></a></li>
+                <li class="flex"><a href="#" class="flex"><img
+                            src="https://yousangyeop.github.io/img1/blog/article/2020/heart.png" alt=""></a></li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <div class="con bn">
+        <div class="bn-box">
+            <img src="https://yousangyeop.github.io/img1/pf/gm/main_banner_gw%20(1).jpg" alt="">
+            <ul>
+                <li>GENTLE MONSTER X KRIS WU</li>
+                <ul class="text">
+                    <li>젠틀몬스터와 글로벌 아티스트 KRIS WU의 협업으로</li>
+                    <li>이루어진 GENTLE WU 컬렉션을 소개합니다.</li>
+                </ul>
+                <div class="btn-1"><a href="#">자세히 보기</a></div>
+                <div class="btn-2"><a href="#">컬렉션 구매하기</a></div>
+            </ul>
+
+        </div>
+    </div>
+</body>
+
+</html>
