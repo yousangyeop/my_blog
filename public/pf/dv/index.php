eec41aa7d973cd73567f7cd8da28d55115c8aae2
yousangyeop <yeop4030@naver.com>
2020-07-13 18:38:26 +0900

    블로그 포트폴리오 작업2

---
+++ public/pf/dv/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>다인비주얼</title>
+    <link rel="stylesheet" href="./index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <script src="./index.js"></script>
+</head>
+
+<body>
+    <div class="menu-box con flex">
+        <div class="flex flex-ai-c">
+            <img src="http://www.di-in.co.kr/img/logo.png" alt="">
+        </div>
+        <div class="menu-bar flex flex flex-ai-c flex-jc-end flex-grow-1 ">
+            <ul class="flex flex-jc-end">
+                <li><a href="#" class="block"></a>About</li>
+                <li><a href="#" class="block"></a>Service</li>
+                <li><a href="#" class="block"></a>Portfolio</li>
+                <li><a href="#" class="block"></a>Contact</li>
+            </ul>
+        </div>
+    </div>
+    <div class="my-slider-1 con">
+        <div class="owl-carousel owl-theme">
+            <div class="item slide-1">
+                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-01-con-b.png" alt="">
+                </div>
+            </div>
+            <div class="item slide-2">
+                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-02-con-b.png" alt="">
+                </div>
+            </div>
+            <div class="item slide-3">
+                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-03-con-b.png" alt="">
+                </div>
+            </div>
+            <div class="item slide-4">
+                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-04-con-b.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
--- public/pf/gm/index.php
+++ public/pf/gm/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>젠틀몬스터</title>
     <link rel="stylesheet" href="./index.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     
