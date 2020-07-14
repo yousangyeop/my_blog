cb7fdef47b208e10f9f2486d37f1b91ee50b3700
yousangyeop <yeop4030@naver.com>
2020-07-14 18:37:13 +0900

    다인비주얼

--- public/pf/dv/index.php
+++ public/pf/dv/index.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <script src="./index.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -20,7 +21,7 @@
         <div class="flex flex-ai-c">
             <img src="http://www.di-in.co.kr/img/logo.png" alt="">
         </div>
-        <div class="menu-bar flex flex flex-ai-c flex-jc-end flex-grow-1 ">
+        <div class="menu-bar flex flex-ai-c flex-jc-end flex-grow-1 ">
             <ul class="flex flex-jc-end">
                 <li><a href="#" class="block"></a>About</li>
                 <li><a href="#" class="block"></a>Service</li>
@@ -29,24 +30,69 @@
             </ul>
         </div>
     </div>
-    <div class="my-slider-1 con">
-        <div class="owl-carousel owl-theme">
-            <div class="item slide-1">
-                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-01-con-b.png" alt="">
-                </div>
+    <div class="sbs-slider">
+        <div class="slides">
+            <div class="active slide1">
+                <div class="text1"><img src="http://www.di-in.co.kr/img/new2020/main-visual-01-con-t.png" alt=""></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-01-con-b.png" alt="">
             </div>
-            <div class="item slide-2">
-                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-02-con-b.png" alt="">
-                </div>
+            <div class="slide2">
+                <div class="text2"><img src="http://www.di-in.co.kr/img/new2020/main-visual-02-con-t.png" alt=""></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-02-con-b.png" alt="">
             </div>
-            <div class="item slide-3">
-                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-03-con-b.png" alt="">
-                </div>
+            <div class="slide3">
+                <div class="text3"><img src="http://www.di-in.co.kr/img/new2020/main-visual-03-con-t.png" alt=""></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-03-con-b.png" alt="">
             </div>
-            <div class="item slide-4">
-                <div class="img-box"><img src="http://www.di-in.co.kr/img/new2020/main-visual-04-con-b.png" alt="">
-                </div>
+            <div class="slide4">
+                <div class="text4"><img src="http://www.di-in.co.kr/img/new2020/main-visual-04-con-t.png" alt=""></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-04-con-b.png" alt="">
             </div>
+        </div>
+
+        <div class="side-bars">
+            <div>
+                <span>
+                    <img src="http://www.di-in.co.kr/img/new2020/btn-main-v-prev.png" alt="">
+                </span>
+            </div>
+            <div>
+                <span>
+                    <img src="http://www.di-in.co.kr/img/new2020/btn-main-v-next.png" alt="">
+                </span>
+            </div>
+        </div>
+        <div class="btn-slide flex flex-jc-sb">
+            <div class="btn1 flex fle-jc-sb active">
+                <div class="dot ca"></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-dot-01.png" alt="">
+            </div>
+            <div class="btn2 flex">
+                <div class="dot"></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-dot-02.png" alt="">
+            </div>
+            <div class="btn3 flex">
+                <div class="dot"></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-dot-03.png" alt="">
+            </div>
+            <div class="btn4 flex">
+                <div class="dot"></div>
+                <img src="http://www.di-in.co.kr/img/new2020/main-visual-dot-04.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="bn-bar con">
+        <div class="bn-text">
+            Service
+        </div>
+        <div class="bn-text-2">
+            디지털 컨텐츠 제작 부터 홍보 마케팅까지 통합 서비스를 제공합니다.
+        </div>
+        <div class=".bn-image flex flex-jc-c">
+            <div class="image-1 flex"><img src="http://www.di-in.co.kr/img/new2020/main-service-1.png" alt=""></div>
+            <div class="image-2 flex"><img src="http://www.di-in.co.kr/img/new2020/main-service-2.png" alt=""></div>
+            <div class="image-3 flex"><img src="http://www.di-in.co.kr/img/new2020/main-service-3.png" alt=""></div>
+            <div class="image-4 flex"><img src="http://www.di-in.co.kr/img/new2020/main-service-4.png" alt=""></div>
         </div>
     </div>
 
