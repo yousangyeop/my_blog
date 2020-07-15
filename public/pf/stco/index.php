a0cf0e4c622c4879a363e2d4d55e600604e86ef1
yousangyeop <yeop4030@naver.com>
2020-07-15 21:22:03 +0900

    stco 사이트 작업

--- public/pf/stco/index.php
+++ public/pf/stco/index.php
@@ -4,11 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>stco</title>
-=======
-    <title>STCO</title>
->>>>>>> f3246158d8392e6c1cd28b87cb0970d844f29161
     <link rel="stylesheet" href="./index.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="./index.js"></script>
@@ -16,7 +12,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD
     <div class="con flex-ai-c">
         <ul class="flex left-menu flex-1-0-0">
             <li class="flex">
@@ -41,53 +36,34 @@
             <li class="flex"><a href="#">고객센터</a></li>
         </ul>
     </div>
-    <div class="bn-bar">
-        <ul>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
-            <li><img src="" alt=""></li>
+    <div class="sbs-slider">
+        <ul class="slides">
+            <li class="active slide1"><img src="http://localhost:8077/resource/img/stco-bn-1.jpg" alt=""></li>
+            <li class="slide2"><img src="http://localhost:8077/resource/img/stco-bn-2.jpg" alt=""></li>
+            <li class="slide3"><img src="http://localhost:8077/resource/img/stco-bn-3.jpg" alt=""></li>
+            <li class="slide4"><img src="http://localhost:8077/resource/img/stco-bn-4.jpg" alt=""></li>
+            <li class="slide5"><img src="http://localhost:8077/resource/img/stco-bn-5.jpg" alt=""></li>
+            <li class="slide6"><img src="http://localhost:8077/resource/img/stco-bn-6.jpg" alt=""></li>
+            <li class="slide7"><img src="http://localhost:8077/resource/img/stco-bn-7.jpg" alt=""></li>
+            <li class="slide8"><img src="http://localhost:8077/resource/img/stco-bn-8.jpg" alt=""></li>
+            <li class="slide9"><img src="http://localhost:8077/resource/img/stco-bn-9.jpg" alt=""></li>
+            <li class="slide10"><img src="http://localhost:8077/resource/img/stco-bn-10.jpg" alt=""></li>
+            <li class="slide11"><img src="http://localhost:8077/resource/img/stco-bn-11.jpg" alt=""></li>
+            <li class="slide12"><img src="http://localhost:8077/resource/img/stco-bn-12.jpg" alt=""></li>
         </ul>
     </div>
-=======
-    <div class="con">
-        <ul class="left-menu flex">
-            <li>
-                <a href="#"><img src="http://localhost:8077/resource/img/toggle-bar.png" alt="">
-                <span>전체메뉴</span>
-                </a>
-            </li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <div class="main flex -jc-c">
-            <a href="#">STCO ONLINE MALL</a>
+    <div class="side-bars">
+            <div>
+                <span>
+                    <img src="http://www.di-in.co.kr/img/new2020/btn-main-v-prev.png" alt="">
+                </span>
+            </div>
+            <div>
+                <span>
+                    <img src="http://www.di-in.co.kr/img/new2020/btn-main-v-next.png" alt="">
+                </span>
+            </div>
         </div>
-        <ul class="right-menu flex fle-jc-end">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </div>
-
-
-
-
-
-
-
->>>>>>> f3246158d8392e6c1cd28b87cb0970d844f29161
 </body>
 
 </html>
