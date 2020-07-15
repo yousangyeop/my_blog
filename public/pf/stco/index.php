f3246158d8392e6c1cd28b87cb0970d844f29161
yousangyeop <yeop4030@naver.com>
2020-07-15 15:36:27 +0900

    돋보기 이미지 추가

--- public/pf/stco/index.php
+++ public/pf/stco/index.php
@@ -13,19 +13,21 @@
 
 <body>
     <div class="con">
-        <ul class="left-menu">
+        <ul class="left-menu flex">
             <li>
-                
+                <a href="#"><img src="http://localhost:8077/resource/img/toggle-bar.png" alt="">
+                <span>전체메뉴</span>
+                </a>
             </li>
             <li></li>
             <li></li>
             <li></li>
             <li></li>
         </ul>
-        <div class="main">
-        <a href="#">STCO ONLINE MALL</a>
+        <div class="main flex -jc-c">
+            <a href="#">STCO ONLINE MALL</a>
         </div>
-        <ul class="right-menu">
+        <ul class="right-menu flex fle-jc-end">
             <li></li>
             <li></li>
             <li></li>
