1d84876c120d602b1eb7dcd86f4f29662c5bbdf1
yousangyeop <yeop4030@naver.com>
2020-07-15 21:07:23 +0900

    stco 이미지 추가

---
+++ public/pf/stco/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>stco</title>
+    <link rel="stylesheet" href="./index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./index.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="con flex-ai-c">
+        <ul class="flex left-menu flex-1-0-0">
+            <li class="flex">
+                <img src="http://localhost:8077/resource/img/toggle-bar.png" alt="" width="27">
+                <a href="#">전체메뉴</a>
+            </li>
+            <li class="flex"><a href="#">아울렛</a></li>
+            <li class="flex"><a href="#">Editor's PICK</a></li>
+            <li class="flex"><a href="#">사이즈추천</a></li>
+            <li class="flex">
+                <img src="http://localhost:8077/resource/img/look.png" alt="" width="25">
+                <a href="#">메뉴</a></li>
+        </ul>
+        <div class="main flex flex-jc-c flex-1-0-0">
+            <h1 class="flex">STCO</h1>
+            <h4 class="flex">ONLINE MALL</h4>
+        </div>
+        <ul class="right-menu flex flex-1-0-0 flex-jc-end">
+            <li class="flex"><a href="#">로그인</a></li>
+            <li class="flex"><a href="#">회원가입</a></li>
+            <li class="flex"><a href="#">장바구니</a></li>
+            <li class="flex"><a href="#">고객센터</a></li>
+        </ul>
+    </div>
+    <div class="bn-bar">
+        <ul>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+        </ul>
+    </div>
+</body>
+
+</html>
