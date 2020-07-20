16373eb081eefb086be4b9fa68afa894c082224c
yousangyeop <yeop4030@naver.com>
2020-07-20 18:35:46 +0900

    포트폴리오 한국쉬즈라인 작업중

---
+++ public/pf/shes/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>한국쉬즈라인</title>
+    <script src="./index.js"></script>
+    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+</head>
+
+<body>
+    <nav class="flex menu-bar">
+        <div class="con flex menu-box flex-ai-c">
+            <ul class="flex flex-grow-1 left-menu">
+                <li class="flex flex-1-0-0"><a href="#">쉬즈라인</a></li>
+                <li class="flex flex-1-0-0"><a href="#">브랜드 소개</a></li>
+                <li class="flex flex-1-0-0"><a href="#">제품소개</a></li>
+            </ul>
+            <div class="img-box logo">
+                <a href="#"><img src="http://shes-line.co.kr/static/images/common/logo.jpg" alt=""></a>
+            </div>
+            <ul class="flex flex-grow-1 flex-jc-end right-menu">
+                <li class="flex flex-jc-end"><a href="#">매장안내</a></li>
+                <li class="flex flex-jc-end"><a href="#">뉴스/홍보</a></li>
+                <ul class="lan">
+                    <li class="kr">
+                        <span>KR</span>
+                        <span>▼</span>
+                    </li>
+                    <li class="en">
+                        <span>EN</span>
+                        <span>▼</span>
+                    </li>
+                    <li class="ch">
+                        <span>Ch</span>
+                        <span>▼</span>
+                    </li>
+                </ul>
+                <li class="toggle">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="slider">
+        <div class="slides">
+            <div class="slide1 active">
+                <img src="http://shes-line.co.kr/_upfiles/main/2018081416551_main.jpg" alt="">
+            </div>
+            <div class="slide2">
+                <img src="http://shes-line.co.kr/_upfiles/main/20180822155632_main.jpg" alt="">
+            </div>
+            <div class="slide3">
+                <img src="http://shes-line.co.kr/_upfiles/main/20181116201222_main.jpg" alt="">
+            </div>
+        </div>
+        <div class="slide-bars">
+            <div>
+                <i class="fas fa-angle-left"></i>
+                <i class="fas fa-angle-right"></i>
+            </div>
+        </div>
+        <div class="btn-slide flex flex-jc-sb">
+            <div class="btn1 flex fle-jc-sb active">
+                <div class="dot"></div>
+                <i class="fas fa-circle"></i>
+            </div>
+            <div class="btn2 flex">
+                <div class="dot"></div>
+                <i class="fas fa-circle"></i>
+            </div>
+            <div class="btn3 flex">
+                <div class="dot"></div>
+                <i class="fas fa-circle"></i>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
