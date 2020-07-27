2c086f4e7af01fdbd10657c41d372e9cfa2ae122
yousangyeop <yeop4030@naver.com>
2020-07-27 15:38:59 +0900

    포트폴리오

---
+++ public/pf/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>포트폴리오</title>
+</head>
+
+<body>
+    <h1>포트폴리오</h1>
+    <ul>
+        <li><a href="./gm/index.php">젠틀몬스터</a></li>
+        <li><a href="./dv/index.php">디인비주얼</a></li>
+        <li><a href="./stco/index.php">stco</a></li>
+        <li><a href="./shes/index.php">한국쉬즈라인</a></li>
+    </ul>
+    <h1>디자인</h1>
+    <ul>
+        <li><a href="https://yousangyeop.github.io/img1/pf/artworks/1/1.png">잔 받침</a></li>
+    </ul>
+
+</body>
+
+</html>
