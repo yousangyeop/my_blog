9760fd0587951948dab305aa4b8ce47731de0d3a
yousangyeop <yeop4030@naver.com>
2020-07-28 21:27:05 +0900

    yp 슬라이드 첫번째 시도 실패

---
+++ public/pf/yp/index.js
@@ -0,0 +1,30 @@
+function dotSlide() {
+    var $this = $(this);
+    var $thisIndex = $thhis.index('.slider');
+    var $slider = $this.closest('.slider');
+    var $slides = $slider.find('>.slides > li');
+    var $activeDot = $slider.find('> .slides > .active');
+    var $activeSlide = $slider.find('> .slides > .active');
+    var $slideIndex = $slides.eq($thisIndex);
+
+
+    $activeDot.removeClass('active');
+    $activeSlide.removeClass('active');
+
+    $this.addClass('active');
+    $slideIndex.addClass('active');
+}
+
+function dotSlide__init() {
+    $('.slide-btn > li').click(dotSlide);
+}
+function setInt__init() {
+    setInterval(function() {
+        $('.slider > .slide-btn > li:last-child').click()
+    }, 2000)
+}
+
+$(function () {
+    dotSlide__init();
+    setInt__init();
+});
--- public/pf/yp/index.php
+++ public/pf/yp/index.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>yogerpresso</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="./index.css">
-    <script src="index.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./index.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -53,19 +54,28 @@
     </div>
     <div class="bn-bar flex flex-jc-sb">
         <ul class="bn-box1 flex flex-ai-c">
-            <li class="flex flex-jc-sb"><span>공지사항</span>
+            <li class="flex flex-jc-sa con">
+                <span>공지사항</span>
                 <a href="#">
                     [오픈] 1417th 해운대엘시티점 OPEN</a>
                 <img src="https://www.yogerpresso.co.kr/skin/img/new_mainv4/btn_more2_white.png" alt="">
             </li>
         </ul>
-        <ul class="bn-box2">
-            <li><span>언론보도</span></li>
-            <li><a href="#">요거프레소, 여름엔 기분 좋은 깔끔함 '피치 스윗 얼그레이티'
-                </a></li>
-            <li><img src="https://www.yogerpresso.co.kr/skin/img/new_mainv4/btn_more2.png" alt="">
+        <ul class="bn-box2 flex flex-ai-c">
+            <li class="flex flex-jc-sa con">
+                <span>언론보도</span>
+                <a href="#">요거프레소, 여름엔 기분 좋은 깔끔함 '피..</a>
+                <img src="https://www.yogerpresso.co.kr/skin/img/new_mainv4/btn_more2.png" alt="">
             </li>
         </ul>
+    </div>
+    <div class="bn-bar-2">
+        <nav class="con">
+            <ul class="img1"><img src="https://yousangyeop.github.io/img1/pf/yp/4.jpg" alt="">
+                <li class="img1"><img src="https://yousangyeop.github.io/img1/pf/yp/5.png" alt=""></li>
+                <li class="img2"><a href="#"><img src="https://yousangyeop.github.io/img1/pf/yp/6.png" alt=""></a></li>
+            </ul>
+        </nav>
     </div>
 </body>
 
