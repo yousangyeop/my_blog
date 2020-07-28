f9588f0747eb396749adf75edfc15c35ef960951
yousangyeop <yeop4030@naver.com>
2020-07-28 18:50:18 +0900

    yp포트폴리오 작업중

---
+++ public/pf/yp/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yogerpresso</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="./index.css">
+    <script src="index.js"></script>
+</head>
+
+<body>
+    <nav class="top-bar con flex flex-jc-sb flex-ai-c">
+        <div class="info"><a href="#">개설안내 홈페이지</a></span>
+        </div>
+        <div class="top-menu flex-jc-sb">
+            <ul class="login flex">
+                <li><a href="#">로그인</a></li>
+                <li><a href="#">회원가입</a></li>
+                <li><a href="#">매장찾기</a></li>
+            </ul>
+            <ul class="sns flex flex-jc-sb">
+                <li class="flex"><a href="#"><img src="https://www.yogerpresso.co.kr/skin/img/brand/ico_sns1.gif"
+                            alt=""></a></li>
+                <li class="flex"><a href="#"><img src="https://www.yogerpresso.co.kr/skin/img/brand/ico_sns2.gif"
+                            alt=""></a></li>
+                <li class="flex"><a href="#"><img src="https://www.yogerpresso.co.kr/skin/img/brand/ico_sns3.gif"
+                            alt=""></a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="menu-bar con flex flex-jc-sb">
+        <div class="logo flex-ai-c">
+            <img src="https://www.yogerpresso.co.kr/skin/img/brand/new_top_logo.png" alt=""></div>
+        <ul class="menu-box flex">
+            <li class="menu"><a href="#">메뉴 안내</a></li>
+            <li class="joy"><a href="#">즐기기</a></li>
+            <li class="store"><a href="#">매장 안내</a></li>
+            <li class="news"><a href="#">뉴스</a></li>
+        </ul>
+    </div>
+    <div class="slider">
+        <ul class="slides">
+            <li class="active"><img src="https://yousangyeop.github.io/img1/pf/yp/1.jpg" alt=""></li>
+            <li><img src="https://yousangyeop.github.io/img1/pf/yp/2.jpg" alt=""></li>
+            <li><img src="https://yousangyeop.github.io/img1/pf/yp/3.jpg" alt=""></li>
+        </ul>
+        <ul class="slide-btn flex">
+            <li class="active"></li>
+            <li></li>
+            <li></li>
+    </div>
+    <div class="bn-bar flex flex-jc-sb">
+        <ul class="bn-box1 flex flex-ai-c">
+            <li class="flex flex-jc-sb"><span>공지사항</span>
+                <a href="#">
+                    [오픈] 1417th 해운대엘시티점 OPEN</a>
+                <img src="https://www.yogerpresso.co.kr/skin/img/new_mainv4/btn_more2_white.png" alt="">
+            </li>
+        </ul>
+        <ul class="bn-box2">
+            <li><span>언론보도</span></li>
+            <li><a href="#">요거프레소, 여름엔 기분 좋은 깔끔함 '피치 스윗 얼그레이티'
+                </a></li>
+            <li><img src="https://www.yogerpresso.co.kr/skin/img/new_mainv4/btn_more2.png" alt="">
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html>
