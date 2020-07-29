34dfa1b07156395f181f1526e806cd6edc8328c9
yousangyeop <yeop4030@naver.com>
2020-07-29 18:37:17 +0900

    yp 슬라이드 완성

--- public/pf/yp/index.js
+++ public/pf/yp/index.js
@@ -1,30 +1,36 @@
-function dotSlide() {
+
+  function dotSlide(){
     var $this = $(this);
-    var $thisIndex = $thhis.index('.slider');
+    var $thisIndex = $this.index();
     var $slider = $this.closest('.slider');
-    var $slides = $slider.find('>.slides > li');
-    var $activeDot = $slider.find('> .slides > .active');
+    var $slides = $slider.find('> .slides > li');
+    var $activeDot = $slider.find('> .slide-btn > .active');
     var $activeSlide = $slider.find('> .slides > .active');
     var $slideIndex = $slides.eq($thisIndex);
-
-
+    
+    
+  
     $activeDot.removeClass('active');
     $activeSlide.removeClass('active');
-
+  
     $this.addClass('active');
     $slideIndex.addClass('active');
-}
-
-function dotSlide__init() {
+  }
+  
+  
+  function dotSlide__init(){
     $('.slide-btn > li').click(dotSlide);
-}
-function setInt__init() {
-    setInterval(function() {
-        $('.slider > .slide-btn > li:last-child').click()
+  }
+  
+  function setInt__init(){
+    setInterval(function(){
+      $('.slider > .slide-btn > li:last-child').click()
     }, 2000)
-}
-
-$(function () {
+  }
+  
+  
+  
+  $(function () {
     dotSlide__init();
-    setInt__init();
-});
+  });
+  
--- public/pf/yp/index.php
+++ public/pf/yp/index.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>yogerpresso</title>
-    <link rel="stylesheet" href="./index.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="./index.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="./index.js"></script>
+    <link rel="stylesheet" href="./index.css">
 </head>
 
 <body>
