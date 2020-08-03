f432d4d3601d6a8316f2cb23c82a33c1b8cd2c41
yousangyeop <yeop4030@naver.com>
2020-08-03 18:23:05 +0900

    yp블로그 완성

--- public/pf/yp/index.php
+++ public/pf/yp/index.php
@@ -31,15 +31,52 @@
             </ul>
         </div>
     </nav>
-    <div class="menu-bar con flex flex-jc-sb">
-        <div class="logo flex-ai-c">
-            <img src="https://www.yogerpresso.co.kr/skin/img/brand/new_top_logo.png" alt=""></div>
-        <ul class="menu-box flex">
-            <li class="menu"><a href="#">메뉴 안내</a></li>
-            <li class="joy"><a href="#">즐기기</a></li>
-            <li class="store"><a href="#">매장 안내</a></li>
-            <li class="news"><a href="#">뉴스</a></li>
-        </ul>
+    <div class="menu-bar flex flex-jc-sb">
+        <section class="header">
+            <div class="logo flex-ai-c">
+                <img src="https://www.yogerpresso.co.kr/skin/img/brand/new_top_logo.png" alt=""></div>
+            <nav class="gnb">
+                <ul class="menu-box row">
+                    <li class="menu cell">
+                        <a href="#" class="dp1">메뉴 안내</a>
+                        <ul>
+                            <li><a href="#">이달의신메뉴</a></li>
+                            <li><a href="#">커피</a></li>
+                            <li><a href="#">드링크</a></li>
+                            <li><a href="#">요거트</a></li>
+                            <li><a href="#">밀크</a></li>
+                            <li><a href="#">디저트</a></li>
+                            <li><a href="#">카페너리</a></li>
+                            <li><a href="#">MD</a></li>
+                        </ul>
+                    </li>
+                    <li class="joy cell">
+                        <a href="#" class="dp1">즐기기</a>
+                        <ul>
+                            <li><a href="#">원데이클래스</a></li>
+                            <li><a href="#">매거진</a></li>
+                            <li><a href="#">이벤트</a></li>
+                            <li><a href="#">멤버쉽</a></li>
+                        </ul>
+                    </li>
+                    <li class="store cell">
+                        <a href="#" class="dp1">매장 안내</a>
+                        <ul>
+                            <li><a href="#">매장안내</a></li>
+                            <li><a href="#">오픈예정점</a></li>
+                        </ul>
+                    </li>
+                    <li class="news cell">
+                        <a href="#" class="dp1">뉴스</a>
+                        <ul>
+                            <li><a href="#">공지사항</a></li>
+                            <li><a href="#">언론보도</a></li>
+                            <li><a href="#">채용정보</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </section>
     </div>
     <div class="slider">
         <ul class="slides">
@@ -105,11 +142,97 @@
     <div class="bn-bar-4 con">
         <nav class="img">
             <img src="https://yousangyeop.github.io/img1/pf/yp/2-1.jpg" alt="">
-            <ul>
-                <li class="go"><img src="https://yousangyeop.github.io/img1/pf/yp/2-2.png" alt=""></li>
-                <li class="info"><img src="https://yousangyeop.github.io/img1/pf/yp/2-3.png" alt=""></li>
+            <ul class="">
+                <li class="go"><img src="https://yousangyeop.github.io/img1/pf/yp/2-3.png" alt=""></li>
+                <li class="info"><img src="https://yousangyeop.github.io/img1/pf/yp/2-2.png" alt=""></li>
             </ul>
         </nav>
+    </div>
+    <ul class="bn-bar-5 flex flex-jc-sb con">
+        <li class="flex">
+            <a href="#"><img src="https://www.yogerpresso.co.kr/file/oneday_thumbnail/oneday_20200626151418_detail.jpg"
+                    alt=""></a>
+        </li>
+        <li class="flex">
+            <a href="#"><img
+                    src="https://www.yogerpresso.co.kr/file/board/pc_main_banner_%EC%86%8D%EC%B4%88%EC%A4%91%EC%95%99%EC%A0%90.jpg"
+                    alt=""></a>
+        </li>
+    </ul>
+    <div class="line con"></div>
+    <div class="insta con">
+        <ul>
+            <h1>인스타그램</h1>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/116219424_609170180022630_8363168290587188862_n.jpg?_nc_cat=105&_nc_sid=8ae9d6&_nc_ohc=mLAn5TWaGlcAX-GAlQ-&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=cc14b3fba122e84ed2e49bd488148155&oe=5F4D337D"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/116072655_747312502734800_6984626117027890740_n.jpg?_nc_cat=102&_nc_sid=8ae9d6&_nc_ohc=AfwQQfE2J04AX9CE-jY&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=8b212bfb1fbab437461e7ae9d07e32a6&oe=5F4CF95C"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/115909538_936217683560049_5447678848575212078_n.jpg?_nc_cat=110&_nc_sid=8ae9d6&_nc_ohc=MfY4x3tIvhoAX_erRui&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=fa136fcb4790a4d22d2c44f4c6a4ba35&oe=5F4B48EA"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/116284901_287432129028353_8725632495474686318_n.jpg?_nc_cat=104&_nc_sid=8ae9d6&_nc_ohc=syN5abNDcjQAX8LHp0n&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=5d82ac7a32f8d9dce9dce74da8a64c93&oe=5F4D3552"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/116108856_159747805686898_1813524072792446798_n.jpg?_nc_cat=105&_nc_sid=8ae9d6&_nc_ohc=XAU1W1vL-EoAX82q_gN&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=3d5ddeca5dad666c97ce6bfb2f7ce163&oe=5F4DB89E"
+                        alt="" srcset=""></a></li>
+        </ul>
+        <ul>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/115824142_287335065705380_832276259349243476_n.jpg?_nc_cat=108&_nc_sid=8ae9d6&_nc_ohc=xMUkKqX5LJMAX-LiMNI&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=4e99b2febf620bba4fc2345883498352&oe=5F4DC66F"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/116338154_184304353042317_636111197924464234_n.jpg?_nc_cat=110&_nc_sid=8ae9d6&_nc_ohc=Yn9bxsYdjK0AX-QEwaU&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=bbea3ea7b4036090eb29d79f3f87207a&oe=5F4D81F0"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/115849919_3071776816210007_3809278752097190583_n.jpg?_nc_cat=104&_nc_sid=8ae9d6&_nc_ohc=6ZK7AEYJBgMAX916Os6&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=2862cd5d0c1c6e8590d666c0d85c134e&oe=5F4C9223"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/110363864_289706352376570_2430600419367624618_n.jpg?_nc_cat=101&_nc_sid=8ae9d6&_nc_ohc=vxTcQFZUURsAX8Bgo5h&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=589c68fc50bb938d48928dc352f4c555&oe=5F4E449A"
+                        alt="" srcset=""></a></li>
+            <li><a href="#"><img
+                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/109465385_659219964666554_5846375749895158765_n.jpg?_nc_cat=110&_nc_sid=8ae9d6&_nc_ohc=R8jO4bpdOcMAX8SCMLA&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=0bab00c7abde6fa5f8fef8825c2c477c&oe=5F4DBC4B"
+                        alt="" srcset=""></a></li>
+        </ul>
+    </div>
+    <div class="line-1"></div>
+    <div class="pm">
+        <ul>
+            <li><a href="#"><img src="https://www.yogerpresso.co.kr/skin/img/new_mainv4/200401_awards_pc_footer.PNG"
+                        alt=""></a></li>
+        </ul>
+    </div>
+    <div class="bottom-bar">
+        <div class="foot">
+            <ul>
+                <li><a href="#">브랜드 소개</a></li>
+                <li><a href="#">가맹점문의</a></li>
+                <li><a href="#">오시는길</a></li>
+                <li><a href="#">고객의소리</a></li>
+                <li><a href="#">사이트이용약관</a></li>
+                <li><a href="#">개인정보처리방침</a></li>
+            </ul>
+        </div>
+        <div class="logo">
+            <img src="https://www.yogerpresso.co.kr/skin/img/brand/new_top_logo.png" alt="">
+        </div>
+        <div class="yp-info">
+            <ul>
+                <li>사업자 등록번호 : 109-86-10925</li>
+                <li>대표전화 : 02-2065-0776</li>
+                <li>FAX : 02-2065-7161</li>
+                <li>개설문의 : 1588-0738</li>
+            </ul>
+            <ul>
+                <li>서울 본사 : 서울특별시 마포구 월드컵북로6길 3 이노베이스 빌딩 4, 5층 (주)요거프레소</li>
+            </ul>
+            <ul>
+                <li>바리스타 아카데미 : 서울특별시 마포구 월드컵북로6길 3 이노베이스 빌딩 4층</li>
+            </ul>
+        </div>
+        <div class="copy">COPYRIGHT(C) YOGERPRESSO. ALL RIGHTS RESERVED.</div>
     </div>
 
 </body>
