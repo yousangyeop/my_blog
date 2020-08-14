72df4e5d45b7d749bc364d7c455dd2c758f72820
yousangyeop <yeop4030@naver.com>
2020-08-14 18:35:19 +0900

    폴로스터즈 작업중

--- public/pf/pr/public/index.php
+++ public/pf/pr/public/index.php
@@ -8,27 +8,27 @@ include "../part/head.php"
 <div id="fullpage">
     <div class="section page1" style="background-image:url('https://yousangyeop.github.io/img1/pr_img/1page.png')">
         <div class="txt1">
-            좋은 커피를 알리기 위한1
+            좋은 커피를 알리기 위한
         </div>
     </div>
     <div class="section page2" style="background-image:url('https://yousangyeop.github.io/img1/pr_img/2page.png')">
         <div class="txt1">
-            좋은 커피를 알리기 위한2
+            가정에서도 신선한 원두를
         </div>
     </div>
     <div class="section page3" style="background-image:url('https://yousangyeop.github.io/img1/pr_img/3page.png')">
         <div class="txt1">
-            좋은 커피를 알리기 위한3
+            전국 어디서나 맛있는 커피를
         </div>
     </div>
     <div class="section page4" style="background-image:url('https://yousangyeop.github.io/img1/pr_img/4page.png')">
         <div class="txt1">
-            좋은 커피를 알리기 위한4
+            좋은 커피 문화를 위한
         </div>
     </div>
     <div class="section page5" style="background-image:url('https://yousangyeop.github.io/img1/pr_img/5page.png')">
         <div class="txt1">
-            좋은 커피를 알리기 위한5
+            폴 로스터즈의 소식은
         </div>
     </div>
 </div>
