deff81717a6ae96b43a9e7281864327ef6e029da
yousangyeop <yeop4030@naver.com>
2020-08-15 14:48:37 +0900

    폴로스터즈 작업중

--- public/pf/pr/public/about.php
+++ public/pf/pr/public/about.php
@@ -6,7 +6,14 @@ include "../part/head.php"
 <link rel="stylesheet" href="../public/onlineShop.css">
 <script src="../public/onlineShop.js"></script>
 
-<h1>about</h1>
+<div class="about">
+    <img src="https://yousangyeop.github.io/img1/pr_img/1page-1.png" alt="">
+</div>
+<ul class="member">
+    <li>김예준<img src="" alt=""></li>
+    <li>장승우<img src="" alt=""></li>
+    <li>유상엽<img src="" alt=""></li>
+</ul>
 
 <?php
 include "../part/foot.php"
