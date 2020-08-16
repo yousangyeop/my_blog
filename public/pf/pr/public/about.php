a68ef91017dd4cf33831da579cbebe5561004df6
yousangyeop <yeop4030@naver.com>
2020-08-16 18:44:48 +0900

    폴 로스터즈 작업중2

--- public/pf/pr/public/about.php
+++ public/pf/pr/public/about.php
@@ -6,13 +6,31 @@ include "../part/head.php"
 <link rel="stylesheet" href="../public/onlineShop.css">
 <script src="../public/onlineShop.js"></script>
 
-<div class="about">
-    <img src="https://yousangyeop.github.io/img1/pr_img/1page-1.png" alt="">
+<div class="about" style="background-image:url('https://yousangyeop.github.io/img1/pr_img/1page-1.png')">
 </div>
 <ul class="member">
     <li>김예준<img src="" alt=""></li>
     <li>장승우<img src="" alt=""></li>
-    <li>유상엽<img src="" alt=""></li>
+    <li>유상엽
+        <br>
+        폴로스터즈에서
+        일하고 있는
+        유상엽입니다.
+        <br>
+        폴로스터즈에서 바리스타와
+        디자이너 겸하고 있습니다.
+        <br>
+        좋은 커피와
+        멋진 디자인을 위해
+        끊임없이 연구하고 있습니다.
+        <br>
+        감사합니다.
+        <br>
+        insta : yeobb
+        <br>
+        email : yeop4030@naver.com
+
+        <img src="" alt=""></li>
 </ul>
 
 <?php
--- public/pf/pr/public/onlineShop.php
+++ public/pf/pr/public/onlineShop.php
@@ -2,11 +2,32 @@
 include "../part/head.php"
 ?>
 
+<link rel="stylesheet" href="../public/common.css">
 <link rel="stylesheet" href="../public/sub.css">
 <link rel="stylesheet" href="../public/onlineShop.css">
 <script src="../public/onlineShop.js"></script>
 
-<h1>온라인 샵</h1>
+<div class="online flex flex-ai-sb">
+    <ul class="blend flex flex-jc-sa">
+        <li class="info flex flex-ai-c">
+            blend <br>:<br> 콜롬비아 : 40% <br> 브라질 : 5% <br> 과테말라 : 20% <br> 케냐 : 5% <br> 에티오피아 : 30%
+        </li>
+        <li class="flex flex-ai-c">cupnote <br>:<br> apple <br> dark chocolate <br> peach <br> Lavender</li>
+        <li class="g100"><img src="https://yousangyeop.github.io/img1/pr_img/100g%20img.png" alt="" width="270"></li>
+        <li class="g200"><img src="https://yousangyeop.github.io/img1/pr_img/200g%20img.png" alt="" width="270"></li>
+    </ul>
+</div>
+<div class="single">
+    <ul class="flex flex-jc-sa">
+        <li class="flex-ai-c">single</li>
+        <li><img src="https://yousangyeop.github.io/img1/pr_img/2page-1.png" alt="">과테말라</li>
+        <li><img src="https://yousangyeop.github.io/img1/pr_img/2page-2.png" alt="">에티오피아 워시드</li>
+        <li><img src="https://yousangyeop.github.io/img1/pr_img/2page-3.png" alt="">에티오피아 내츄럴</li>
+        <li><img src="https://yousangyeop.github.io/img1/pr_img/2page-4.png" alt="">케냐</li>
+    </ul>
+</div>
+<div></div>
+<div></div>
 
 <?php
 include "../part/foot.php"
