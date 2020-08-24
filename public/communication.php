e5eff3907b94ccc0f48b3d755e3bd42ef60fe0bc
yousangyeop <yeop4030@naver.com>
2020-08-24 15:00:30 +0900

    포트폴리오

--- public/communication.php
+++ public/communication.php
@@ -5,15 +5,18 @@ include "../part/head.php"
 <link rel="stylesheet" href="/resource/communication.css">
 
 <div class="communication flex con">
-    <div class="article flex-ai-c">
+    <div class="article flex-ai-c flex-jc-c">
         <img src="../resource/img/blog.logo.png" alt="" width="400">
         <div class="sns">
+        <H1 class="flex">CONTACT</H1>
             <a href="https://www.instagram.com/yeobb/">
                 <img src="../resource/img/insta.png" alt="">
             </a>
             <a href="https://www.facebook.com/profile.php?id=100003823120605">
                 <img src="../resource/img/facebook.png" alt="">
             </a>
+            <a href="https://mail.naver.com/"><img src="https://yousangyeop.github.io/img1/blog/article/2020/mail.png"
+                    alt="" width="75"></a>
         </div>
     </div>
 </div>
