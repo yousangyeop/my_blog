6365bfdb467dda92f64388734c32033329d20c80
yousangyeop <yeop4030@naver.com>
2020-08-25 18:43:29 +0900

    포트폴리오 페이지

---
+++ public/pf/page/pf.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>포트폴리오 페이지</title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="pf.css">
+    <script src="pf.js"></script>
+</head>
+
+<body>
+    <nav>
+        <h1>portfolio</h1>
+        <ul class="port">
+            <li class="gm"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/gm.png" alt="">gm</a></li>
+            <li class="stco"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/stco.png"alt="">stco</a></li>
+            <li class="dv"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/dv.png" alt="">dv</a></li>
+            <li class="yp"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/yp.png" alt="">yp</a></li>
+            <li class="shes"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/shes.png"alt="">shes</a></li>
+            <li class="paul"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/pr.png"alt="">paul</a></li>
+            <li class="blog"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/blog.png"alt="">blog</a></li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
