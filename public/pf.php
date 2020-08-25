fdff99af633a32403c79f71c5e7f98fd40bc0038
yousangyeop <yeop4030@naver.com>
2020-08-25 14:48:10 +0900

    포트폴리오

--- public/pf.php
+++ public/pf.php
@@ -5,18 +5,18 @@ include "../part/head.php"
 <link rel="stylesheet" href="/resource/common.css">
 
 <nav>
-<h1>포트폴리오</h1>
+    <h1>포트폴리오</h1>
     <ul class="port flex flex-jc-c flex-ai-c">
-        <li class="flex gm"><a href="/pf/gm/index.php"><img src="https://yousangyeop.github.io/img1/blog/article/pf/gm.png"
-                    alt=""></a></li>
-        <li class="flex dv"><a href="/pf/dv/index.php"><img src="https://yousangyeop.github.io/img1/blog/article/pf/dv.png"
-                    alt=""></a></li>
+        <li class="flex gm"><a href="/pf/gm/index.php"><img
+                    src="https://yousangyeop.github.io/img1/blog/article/pf/gm.png" alt=""></a></li>
+        <li class="flex dv"><a href="/pf/dv/index.php"><img
+                    src="https://yousangyeop.github.io/img1/blog/article/pf/dv.png" alt=""></a></li>
         <li class="flex stco"><a href="/pf/stco/index.php"><img
                     src="https://yousangyeop.github.io/img1/blog/article/pf/stco.png" alt=""></a></li>
     </ul>
     <ul class="port-1 flex flex-jc-c flex-ai-c">
-        <li class="flex yp"><a href="/pf/yp/index.php"><img src="https://yousangyeop.github.io/img1/blog/article/pf/yp.png"
-                    alt=""></a></li>
+        <li class="flex yp"><a href="/pf/yp/index.php"><img
+                    src="https://yousangyeop.github.io/img1/blog/article/pf/yp.png" alt=""></a></li>
         <li class="flex shes"><a href="/pf/shes/index.php"><img
                     src="https://yousangyeop.github.io/img1/blog/article/pf/shes.png" alt=""></a></li>
         <li class="flex pr"><a href="https://site1.blog.oa.gg/pf/pr/public/index.php"><img
@@ -24,11 +24,13 @@ include "../part/head.php"
     </ul>
     <h1>디자인</h1>
     <ul class="design">
-        <li class="cup"><img src="https://yousangyeop.github.io/img1/blog/article/pf/cup.png" alt="" width="300"></a></li>
-        <li class="draft"><img src="https://yousangyeop.github.io/img1/pr_img/paul.png" alt="" width="500"></a></li>
-        <li class="logo"><img src="https://yousangyeop.github.io/img1/blog/article/pf/logo.png" alt="" width="300"></a></li>
-        <li class="logo-1"><img src="https://yousangyeop.github.io/img1/blog/article/pf/logo-1.png" alt="" width="300"></a></li>
-        <li class="app"><img src="https://yousangyeop.github.io/img1/pf/artworks/1/미술관 사이트 디자인( 앱 ).png" alt="" width="300"></a></li>
+        <li class="cup"><a href="https://yousangyeop.github.io/img1/blog/article/pf/cup.png">잔받침</a>
+        </li>
+        <li class="logo"><a href="https://yousangyeop.github.io/img1/blog/article/pf/logo.png">로고 ver1</a>
+        </li>
+        <li class="logo-1"><a href="https://yousangyeop.github.io/img1/blog/article/pf/logo-1.png">로고 ver2</a></li>
+        <li class="app"><a href="https://yousangyeop.github.io/img1/pf/artworks/1/미술관 사이트 디자인( 앱 ).png">앱 디자인</a></li>
+        <li class="draft"><a href="https://yousangyeop.github.io/img1/pr_img/paul.png">폴 로스터즈 구상</a></li>
     </ul>
 </nav>
 
