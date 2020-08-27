422320d6f9029a429504e148dfb5e623a288c2be
yousangyeop <yeop4030@naver.com>
2020-08-27 16:03:25 +0900

    포트폴리오 페이지 완성

--- public/pf/page/pf.php
+++ public/pf/page/pf.php
@@ -12,43 +12,59 @@
 </head>
 
 <body>
-    <div class="box">
+    <div class="box" style="background-image: url('https://yousangyeop.github.io/img1/pf/page/wall.png');">
         <div class="port">portfolio</div>
+        <div class="button"><img src="https://yousangyeop.github.io/img1/pf/page/button.png" alt="" width="200"></div>
+        <div class="flower"><img src="https://yousangyeop.github.io/img1/pf/page/flower.png" alt="" width="150"></div>
         <ul class="box-type">
             <li class="box-type-1 flex">
                 <div>
                     <a href="/pf/gm/index.php" style="background-color:rgb(180,205,192);" class="gm"><img
-                            src="https://yousangyeop.github.io/img1/blog/article/pf/gm.png" alt=""></a>
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/gm.png" alt="">
+                        <h1>gentle monster</h1>
+                    </a>
                 </div>
                 <div>
                     <a href="/pf/stco/index.php" style="background-color:rgb(193,174,152);" class="stco"><img
-                            src="https://yousangyeop.github.io/img1/blog/article/pf/stco.png" alt=""></a>
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/stco.png" alt="">
+                        <h1>stco</h1>
+                    </a>
                 </div>
             </li>
             <li class="box-type-2 flex">
                 <div>
                     <a href="/pf/dv/index.php" style="background-color:rgb(153,141,120);" class="dv"><img
-                            src="https://yousangyeop.github.io/img1/blog/article/pf/dv.png" alt=""></a>
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/dv.png" alt="">
+                        <h1>di-in visual</h1>
+                    </a>
                 </div>
                 <div>
                     <a href="/pf/yp/index.php" style="background-color:rgb(178,179,189);" class="yp"><img
-                            src="https://yousangyeop.github.io/img1/blog/article/pf/yp.png" alt=""></a>
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/yp.png" alt="">
+                        <h1>yogerpresso</h1>
+                    </a>
                 </div>
             </li>
             <li class="box-type-3">
                 <div>
                     <a href="/pf/shes/index.php" style="background-color:rgb(171,169,167);" class="shes"><img
-                            src="https://yousangyeop.github.io/img1/blog/article/pf/shes.png" alt=""></a>
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/shes.png" alt="">
+                        <h1>shes-line</h1>
+                    </a>
                 </div>
             </li>
             <li class="box-type-4 flex">
                 <div>
-                    <a href="https://site1.blog.oa.gg/pf/pr/public/index.php" style="background-color:rgb(210,190,196);" class="pr"><img
-                            src="https://yousangyeop.github.io/img1/blog/article/pf/pr.png" alt=""></a>
+                    <a href="https://site1.blog.oa.gg/pf/pr/public/index.php" style="background-color:rgb(210,190,196);"
+                        class="pr"><img src="https://yousangyeop.github.io/img1/blog/article/pf/pr.png" alt="">
+                        <h1>paul roasters</h1>
+                    </a>
                 </div>
                 <div>
                     <a href="https://site1.blog.oa.gg/" style="background-color:rgb(193,182,162);" class="blog"><img
-                            src="https://yousangyeop.github.io/img1/blog/article/pf/blog.png" alt=""></a>
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/blog.png" alt="">
+                        <h1>my blog</h1>
+                    </a>
                 </div>
             </li>
         </ul>
