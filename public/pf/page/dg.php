51bcc87c1914a70d670af5fc653130cda6312f51
yousangyeop <yeop4030@naver.com>
2020-08-31 16:52:36 +0900

    페이지 완성

--- public/pf/page/dg.php
+++ public/pf/page/dg.php
@@ -12,18 +12,75 @@
 </head>
 
 <body>
-    <nav>
-        <h1>design</h1>
-        <ul class="port">
-            <li class="gm"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/gm.png" alt="">gm</a></li>
-            <li class="stco"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/stco.png"alt="">stco</a></li>
-            <li class="dv"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/dv.png" alt="">dv</a></li>
-            <li class="yp"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/yp.png" alt="">yp</a></li>
-            <li class="shes"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/shes.png"alt="">shes</a></li>
-            <li class="paul"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/pr.png"alt="">paul</a></li>
-            <li class="blog"><a href=""><img src="https://yousangyeop.github.io/img1/blog/article/pf/blog.png"alt="">blog</a></li>
+    <div class="box" style="background-image: url('https://yousangyeop.github.io/img1/pf/page/wall.png');">
+        <div class="popup1">
+            <div class="box">
+                <img src="https://yousangyeop.github.io/img1/pf/page/contact.png" alt="">
+            </div>
+        </div>
+        <div class="btn-toggle img-box">
+            <img src="https://yousangyeop.github.io/img1/pf/page/button.png" alt="" width="200">
+            <img src="https://yousangyeop.github.io/img1/pf/page/button.png" alt="" width="200">
+        </div>
+        <div class="port">design</div>
+        <div class="flower"><img src="https://yousangyeop.github.io/img1/pf/page/flower.png" alt="" width="100"></div>
+        <ul class="box-type">
+            <li class="box-type-1 flex">
+                <div>
+                    <a href="https://yousangyeop.github.io/img1/blog/article/pf/cup.png" style="background-color:white;"
+                        class="cup"><img src="https://yousangyeop.github.io/img1/blog/article/pf/cup.png" alt="">
+                        <h1>saucer</h1>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://yousangyeop.github.io/img1/blog/article/pf/logo.png"
+                        style="background-color:white;" class="ver1"><img
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/logo.png" alt="">
+                        <h1>logo ver1</h1>
+                    </a>
+                </div>
+            </li>
+            <li class="box-type-2 flex">
+                <div>
+                    <a href="https://yousangyeop.github.io/img1/blog/article/pf/logo-1.png"
+                        style="background-color:white;" class="ver2"><img
+                            src="https://yousangyeop.github.io/img1/blog/article/pf/logo-1.png" alt="">
+                        <h1>logo ver2</h1>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://yousangyeop.github.io/img1/pf/artworks/1/app.png"
+                        style="background-color:white;" class="app"><img
+                            src="https://yousangyeop.github.io/img1/pf/artworks/1/app.png"
+                            alt="">
+                        <h1>app design</h1>
+                    </a>
+                </div>
+            </li>
+            <li class="box-type-3">
+                <div>
+                    <a href="https://yousangyeop.github.io/img1/pr_img/paul.pngg" style="background-color:white;"
+                        class="paul"><img src="https://yousangyeop.github.io/img1/pr_img/paul.png" alt="">
+                        <h1>paul roasters draft</h1>
+                    </a>
+                </div>
+            </li>
+            <li class="box-type-4 flex">
+                <div>
+                    <a href="https://yousangyeop.github.io/img1/pf/page/portfolio.png" style="background-color:white;"
+                        class="pp"><img src="https://yousangyeop.github.io/img1/pf/page/portfolio.png" alt="">
+                        <h1>portfolio page</h1>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://yousangyeop.github.io/img1/pf/page/design.png" style="background-color:white;"
+                        class="dp"><img src="https://yousangyeop.github.io/img1/pf/page/design.png" alt="">
+                        <h1>design page</h1>
+                    </a>
+                </div>
+            </li>
         </ul>
-    </nav>
+    </div>
 
 </body>
 
