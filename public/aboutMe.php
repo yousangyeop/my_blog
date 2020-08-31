aff11a5a37f5d2f7aaac72e1f1d224e25ff22a19
yousangyeop <yeop4030@naver.com>
2020-08-31 18:44:14 +0900

    프로필 페이지 작업중

--- public/aboutMe.php
+++ public/aboutMe.php
@@ -2,15 +2,50 @@
 include "../part/head.php"
 ?>
 <link rel="stylesheet" href="/resource/abouMe.css">
-<div class="about_me flex con">
-    <div class="abata flex-ai-c">
-        <img src="../resource/img/blog.logo.png" alt="" width="400">
-        <div class="me flex flex-jc-c flex-column flex-ai-c">
-            <h1>유상엽에 대해</h1>
-            <h4>대전에 살고 있는 28살 유상엽이라고 합니다.</h4>
-            <h4>저는 패션디자인 자퇴생이지만 여전히 디자인쪽으로 열정 많은 청년입니다.</h4>
-            <h4>현재는 웹디자너를 꿈꾸고 있습니다.</h4>
-        </div>
+<div class="about_me con flex flex-ai-sa">
+    <div class="abata flex">
+        <ul class="">
+            <img src="../resource/img/blog.logo.png" alt="">
+            <li class="name">유상엽 입니다.</li>
+            <li class="">1993 . 04. 30</li>
+            <li class="">010 - 5754 - 2132</li>
+            <li class="">yeop4030@naver.com</li>
+            <li class="">insta : @yeobbook</li>
+        </ul>
+    </div>
+    <div class="profile con flex">
+        <nav class="flex">
+            <div class="profile-1">
+                <ul class="edu">
+                    <li class="title">EDUCATION</li>
+                    <li>- 서대전고등학교 졸업</li>
+                    <li>- 서울모드패션직업전문학교 패션디자인학과 중퇴</li>
+                    <li>- SBS컴퓨터아카데미학원 웹디자인과정 수료</li>
+                </ul>
+                <ul class="cer">
+                    <li class="title">CERTIFICATION</li>
+                    <li>- 자격증</li>
+                    <li>- 자격증</li>
+                </ul>
+                <ul class="skills">
+                    <li class="title">SKILLS</li>
+                    <li><img src="https://yousangyeop.github.io/img1/blog/article/2020/html.png" alt=""></li>
+                    <li><img src="https://yousangyeop.github.io/img1/blog/article/2020/css.png" alt=""></li>
+                    <li><img src="https://yousangyeop.github.io/img1/blog/article/2020/js.png" alt=""></li>
+                    <li><img src="https://yousangyeop.github.io/img1/blog/article/2020/ps.png" alt=""></li>
+                    <li><img src="https://yousangyeop.github.io/img1/blog/article/2020/ai.png" alt=""></li>
+                </ul>
+            </div>
+            <div class="profile-2">
+                <ul class="award">
+                    <li class="title">AWARD</li>
+                    <li>수상</li>
+                </ul>
+                <ul class="actives">
+                    <li class="title">ACTIVES</li>
+                    <li>프로젝트활동</li>
+                </ul>
+            </div>
     </div>
 </div>
 <?php
