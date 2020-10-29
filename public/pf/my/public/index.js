7c3c7ccd2b7d7d2a6ca5339fb9551b75c63a73bc
yousangyeop <yeop4030@naver.com>
2020-10-29 17:01:54 +0900

    3페이지 화살표

--- public/pf/my/public/index.js
+++ public/pf/my/public/index.js
@@ -79,26 +79,7 @@ function Slider__init() {
     });
 }
 
-function Slick__init() {
-    $('.slider-div').slick({
-        slide: 'div', //슬라이드 되어야 할 태그 ex) div, li 
-        infinite: true, //무한 반복 옵션	 
-        slidesToShow: 1, // 한 화면에 보여질 컨텐츠 개수
-        slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
-        speed: 100, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-        arrows: true, // 옆으로 이동하는 화살표 표시 여부
-        dots: false, // 스크롤바 아래 점으로 페이지네이션 여부
-        autoplay: true, // 자동 스크롤 사용 여부
-        autoplaySpeed: 10000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
-        pauseOnHover: true, // 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
-        vertical: true, // 세로 방향 슬라이드 옵션
-        prevArrow: "<button type='button' class='slick-prev'>Previous</button>", // 이전 화살표 모양 설정
-        nextArrow: "", // 다음 화살표 모양 설정
-        dotsClass: "slick-dots", //아래 나오는 페이지네이션(점) css class 지정
-        draggable: false, //드래그 가능 여부 
-        verticalSwiping: true
-    });
-}
+
 
 function select__item() {
     $('.list-box > .item-list > .item').click(function () {
@@ -131,11 +112,8 @@ function popup__view() {
     
 }
 
-
-
 $(function () {
     FullPage__init();
     Slider__init();
-    Slick__init();
     popup__view();
 });
